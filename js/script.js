adf5f918eee88514ea633d8e638a412421b5b0b3
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-08-12 16:35:28 -0700

    logic to save theme state

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 const themeInput = document.querySelector("#custom-toggle");
 
-function changeTheme(e) {
-  const currentVal = parseInt(e.currentTarget.value);
+// - **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and
+// have any additional changes saved in the browser
+function detectColorScheme() {
+  let theme = "dark-theme";
 
-  switch (currentVal) {
-    case 1:
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    theme = "light-theme";
+  }
+
+  switch (theme) {
+    case "dark-theme":
       document.documentElement.classList.remove("light-theme");
       document.documentElement.classList.remove("purple-theme");
-      console.log(currentVal);
       break;
-    case 2:
+    case "light-theme":
       document.documentElement.classList.add("light-theme");
       document.documentElement.classList.remove("purple-theme");
       break;
-    case 3:
+    case "purple-theme":
       document.documentElement.classList.add("purple-theme");
       document.documentElement.classList.remove("light-theme");
       break;
@@ -21,6 +28,34 @@ function changeTheme(e) {
     default:
       break;
   }
+
+  themeInput.value = localStorage.getItem("toggle-val");
 }
 
+function changeTheme(e) {
+  const currentVal = parseInt(e.currentTarget.value);
+
+  localStorage.setItem("toggle-val", currentVal);
+  switch (currentVal) {
+    case 1:
+      document.documentElement.classList.remove("light-theme");
+      document.documentElement.classList.remove("purple-theme");
+      localStorage.setItem("theme", "dark-theme");
+      break;
+    case 2:
+      document.documentElement.classList.add("light-theme");
+      document.documentElement.classList.remove("purple-theme");
+      localStorage.setItem("theme", "light-theme");
+      break;
+    case 3:
+      document.documentElement.classList.add("purple-theme");
+      document.documentElement.classList.remove("light-theme");
+      localStorage.setItem("theme", "purple-theme");
+      break;
+
+    default:
+      break;
+  }
+}
+detectColorScheme();
 themeInput.addEventListener("click", changeTheme);
