e8ff3f8501a06a80d589b18e4b2264564db1a5c3
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-08-14 09:16:47 -0700

    logic for adding numbers

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 const themeInput = document.querySelector("#custom-toggle");
 const numbers = document.querySelectorAll(
-  ".numberpad>div:not(.del, .reset, .equal, .operators)"
+  ".numberpad>button:not(.del, .reset, .equal, .operators)"
 );
-const operators = document.querySelectorAll(".operators");
-const delBtn = document.querySelector(".del");
-const result = document.querySelector(".result");
-const operatorSigns = new Set(["+", "-", "x", "/"]);
+const numberPad = document.querySelector(".numberpad");
 
+console.log(numberPad);
+// const operators = document.querySelectorAll(".operators");
+const delBtn = document.querySelector(".del");
+const display = document.querySelector(".result");
+const equalBtn = document.querySelector(".equal");
+const operatorSigns = new Set(["+", "-", "x", "/"]);
+let expr = "";
+let previousKeyType = "";
+let firstValue = "";
+let operator = "";
 // console.log(delBtn);
 // - Perform mathematical operations like addition, subtraction, multiplication, and division
+
+const operators = { add: "+", subtract: "-", multiply: "*", divide: "/" };
 function numberWithCommas(x) {
   x = x.replace(/,/g, "");
   return parseFloat(x).toLocaleString();
@@ -70,57 +79,151 @@ function changeTheme(e) {
   }
 }
 function updateNumber(e) {
-  result.textContent += e.currentTarget.textContent;
+  if (result.textContent == 0 || previousKeyType === "operator") {
+    result.textContent = e.currentTarget.textContent;
+    previousKeyType = "";
+  } else {
+    result.textContent += e.currentTarget.textContent;
+  }
+  expr = result.textContent;
+  previousKeyType = "number";
 
-  var separators = ["\\+", "-", "\\x", "/"];
+  // let separators = ["\\+", "-", "\\*", "/"];
 
-  var numbers = result.textContent.split(new RegExp(separators.join("|"), "g"));
-  console.log(numbers);
-  //   let array =
-  //   console.log(result.textContent);
-  result.textContent = numberWithCommas(result.textContent);
-  //   console.log(result.textContent);
+  // let numbers = result.textContent.split(new RegExp(separators.join("|"), "g"));
+  // const signs = [...operatorSigns].filter(
+  //   (e) => result.textContent.indexOf(e) != -1
+  // );
+  // result.textContent = "";
+
+  // for (const [idx, number] of numbers.entries()) {
+  //   // console.log(number, idx);
+  //   if (idx % 2 !== 0) {
+  //     console.log(signs);
+  //     result.textContent += signs.pop();
+  //   }
+  //   result.textContent += numberWithCommas(number);
+  // }
 }
 function chooseOperators(e) {
-  console.log(e.currentTarget.textContent);
-  result.textContent += e.currentTarget.textContent;
-  //   switch (e.currentTarget.textContent) {
-  //     case ".":
-  //       dotOperator(e.currentTarget.textContent);
-  //       break;
-  //     case "+":
-  //       plusOperator(e.currentTarget.textContent);
-  //       break;
-  //     case "-":
-  //       minusOperator(e.currentTarget.textContent);
-  //       break;
-  //     case "x":
-  //       timesOperator(e.currentTarget.textContent);
-  //       break;
-  //     case "/":
-  //       divideOperator(e.currentTarget.textContent);
-  //       break;
+  // console.log(e.currentTarget.value);
+  // result.textContent += e.currentTarget.value;
 
-  //     default:
-  //       break;
-  //   }
+  switch (e.currentTarget.value) {
+    case ".":
+      dotOperator(e.currentTarget.value);
+      previousKeyType = "decimal";
+      break;
+
+    case "+":
+      previousKeyType = "operator";
+      firstValue = result.textContent;
+      operator = e.currentTarget.value;
+      break;
+    case "-":
+      previousKeyType = "operator";
+      firstValue = result.textContent;
+      operator = e.currentTarget.value;
+      break;
+    case "*":
+      previousKeyType = "operator";
+      firstValue = result.textContent;
+      operator = e.currentTarget.value;
+      break;
+    case "/":
+      previousKeyType = "operator";
+      firstValue = result.textContent;
+      operator = e.currentTarget.value;
+      break;
+    default:
+      break;
+  }
+  expr = result.textContent;
+  // operator = e.currentTarget.value;
 }
-
+// console.log(math.evaluate("2x4"));
 function dotOperator(dot) {
-  result.textContent += dot;
+  console.log(previousKeyType);
+  if (!result.textContent.includes(".")) {
+    result.textContent += dot;
+  } else if (previousKeyType == "operator") {
+    result.textContent = "0.";
+  }
 }
 function plusOperator(plus) {}
 function minusOperator(minus) {}
 function timesOperator(times) {}
 function divideOperator(divide) {}
+
 detectColorScheme();
+
 themeInput.addEventListener("click", changeTheme);
-for (const number of numbers) {
-  number.addEventListener("click", updateNumber);
-}
-for (const operator of operators) {
-  operator.addEventListener("click", chooseOperators);
-}
-delBtn.addEventListener("click", () => {
-  result.textContent = "0";
+
+numberPad.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+
+    const previousKeyType = numberPad.dataset.previousKeyType;
+    if (!action) {
+      console.log("number-key");
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key");
+      numberPad.dataset.previousKeyType = "operator";
+
+      numberPad.dataset.firstValue = displayedNum;
+      numberPad.dataset.operator = action;
+    }
+
+    if (action === "decimal") {
+      console.log("decimal key");
+      display.textContent = displayedNum + ".";
+    }
+    if (action === "clear") {
+      console.log("clear key");
+    }
+    if (action === "calculate") {
+      console.log("equal key");
+      const firstValue = numberPad.dataset.firstValue;
+      const operator = numberPad.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = math.evaluate(
+        `${firstValue}${operators[operator]}${secondValue}`
+      );
+    }
+    if (action === "delete") {
+      console.log("delete key");
+    }
+  }
 });
+
+// for (const number of numbers) {
+//   number.addEventListener("click", updateNumber);
+// }
+// for (const operator of operators) {
+//   operator.addEventListener("click", chooseOperators);
+// }
+// delBtn.addEventListener("click", () => {
+//   result.textContent = "0";
+//   previousKeyType = "delete";
+// });
+
+// equalBtn.addEventListener("click", () => {
+//   result.textContent = math.evaluate(
+//     `${firstValue}${operator}${result.textContent}`
+//   );
+// });
